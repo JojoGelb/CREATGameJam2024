9278868e83b3a0538a832633d46c571441ed3163
Jordy <jordygelb@gmail.com>
2024-05-31 19:36:33 -0400

    Monkey pushed, jump and stuff

--- Assets/_Script/PlayerController.cs
+++ Assets/_Script/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,6 +10,11 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 moveDir;
 
+    public float jumpForce;
+
+    public float jumpCD = 0;
+    private float jumpTimer = 10000;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -17,17 +23,21 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        jumpTimer += Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.Space) && jumpTimer > jumpCD)
+        {
+            jumpTimer = 0;
+            rb.AddForce(transform.up * jumpForce);
+        }
     }
 
     void FixedUpdate()
     {
+        if(moveDir != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(moveDir.x, moveDir.z) * Mathf.Rad2Deg;
+            transform.GetChild(0).localRotation = Quaternion.Euler(0, angle, 0);
+        }
         rb.MovePosition(rb.position + transform.TransformDirection(moveDir) * moveSpeed * Time.fixedDeltaTime);
-
-        /*rb.MovePosition(rb.position + transform.forward * moveSpeed * Time.fixedDeltaTime);
-        Vector3 yRotation = Vector3.up * rotation * rotationSpeed * Time.fixedDeltaTime;
-        Quaternion deltaRotation = Quaternion.Euler(yRotation);
-        Quaternion targetRotation = rb.rotation * deltaRotation;
-        rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, 50f * Time.deltaTime));*/
-        //transform.Rotate(0f, rotation * rotationSpeed * Time.fixedDeltaTime, 0f, Space.Self);
     }
 }
