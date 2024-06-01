5fc7a4fbf4a65248e0181b97a6525a6f0a0641b2
Jordy <jordygelb@gmail.com>
2024-06-01 01:01:14 -0400

    FF water

--- Assets/_Script/PlayerWaterShooter.cs
+++ Assets/_Script/PlayerWaterShooter.cs
@@ -3,27 +3,30 @@ using UnityEngine;
 
 public class PlayerWaterShooter: MonoBehaviour
 {
+    public ParticleSystem waterGunParticleSystem;
+
     private void Start()
     {
+        waterGunParticleSystem.Stop();
         InputManager.Instance.RegisterToFireEventStarted(OnFireStarted);
         InputManager.Instance.RegisterToFireEventCanceled(OnFireCanceled);
     }
 
     private void OnDisable()
     {
-        InputManager.Instance.UnRegisterToFireEventStarted(OnFireStarted);
-        InputManager.Instance.UnRegisterToFireEventCanceled(OnFireCanceled);
+        InputManager.Instance?.UnRegisterToFireEventStarted(OnFireStarted);
+        InputManager.Instance?.UnRegisterToFireEventCanceled(OnFireCanceled);
     }
     private void OnFireStarted(UnityEngine.InputSystem.InputAction.CallbackContext context)
     {
         //Launch water
-        Debug.Log("Shoot water");
+        waterGunParticleSystem.Play();
     }
 
     private void OnFireCanceled(UnityEngine.InputSystem.InputAction.CallbackContext context)
     {
         //Stop water
-        Debug.Log("Stop water");
+        waterGunParticleSystem.Stop();
     }
 
 
