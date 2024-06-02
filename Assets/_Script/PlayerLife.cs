01b896c44483bf93c4237e2df945950d94229b5b
Jordy <jordygelb@gmail.com>
2024-06-01 20:04:16 -0400

    fixed oilpipes too far from ground level, started player health and end game

---
+++ Assets/_Script/PlayerLife.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //detect under his feet if he touch some poison
+    }
+}
--- Assets/_Script/PlayerWaterShooter.cs
+++ Assets/_Script/PlayerWaterShooter.cs
@@ -7,13 +7,34 @@ public class PlayerWaterShooter: MonoBehaviour
 {
     public VisualEffect waterGunParticleSystem;
 
+    public Rigidbody playerRb;
+    public Transform visualTransform;
+    public float force = 2;
+
+    [Header("Fooling around with jet knockback")]
+    public bool removeMoveSpeedWhenShooting = false;
+    public PlayerController playerController;
+    private float baseMovespeed;
+
+    bool isShooting = false;
+
     List<OilPipeGameplay> oilPipeGameplays = new List<OilPipeGameplay>();
 
     private void Start()
     {
+        baseMovespeed = playerController.moveSpeed;
         waterGunParticleSystem.Stop();
         InputManager.Instance.RegisterToFireEventStarted(OnFireStarted);
         InputManager.Instance.RegisterToFireEventCanceled(OnFireCanceled);
+    }
+
+    private void FixedUpdate()
+    {
+        if(isShooting)
+        {
+            Vector3 direction = -visualTransform.forward;
+            playerRb.AddForce(direction * force, ForceMode.Force);
+        }
     }
 
     private void OnDisable()
@@ -26,6 +47,10 @@ public class PlayerWaterShooter: MonoBehaviour
         //Launch water
         waterGunParticleSystem.Play();
         waterGunParticleSystem.GetComponent<Collider>().enabled = true;
+
+        if(removeMoveSpeedWhenShooting)
+            playerController.moveSpeed = 0;
+        isShooting = true;
     }
 
     private void OnFireCanceled(UnityEngine.InputSystem.InputAction.CallbackContext context)
@@ -38,6 +63,8 @@ public class PlayerWaterShooter: MonoBehaviour
             oil.WaterGunDisabled();
         }
 
+        isShooting = false;
+        playerController.moveSpeed = baseMovespeed;
         oilPipeGameplays.Clear();
     }
 
